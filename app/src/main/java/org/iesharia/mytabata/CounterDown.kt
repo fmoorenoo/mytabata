674aea5c060b18d403154727c14dea9e93138d28
fmoorenoo <fermontemore@gmail.com>
2024-10-17 10:19:28 +0100

    CounterDown arreglado

--- app/src/main/java/org/iesharia/mytabata/CounterDown.kt
+++ app/src/main/java/org/iesharia/mytabata/CounterDown.kt
@@ -2,23 +2,37 @@ package org.iesharia.mytabata
 
 import android.os.CountDownTimer
 import android.util.Log
+import androidx.compose.runtime.Composable
 
-
-class CounterDown (var segundos: Int, var loQueHaceAlHacerTick: (Long) -> Unit) {
-    var myCounter : CountDownTimer
-    var counterState : Boolean = false
-
-    init {
-        myCounter = object : CountDownTimer((segundos * 1000L), 1000) {
-
-            override fun onTick(millisUntilFinished: Long) {
-                loQueHaceAlHacerTick(millisUntilFinished / 1000)
-            }
-
-            override fun onFinish() {
-                counterState = false
-            }
+class CounterDown(var segundos: Int, var loquehacealhacertick: (Long) -> Unit) {
+    private var counterState : Boolean = false
+    val myCounter = object : CountDownTimer((segundos * 1000L), 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            if (counterState) loquehacealhacertick(millisUntilFinished / 1000)
+        }
+        override fun onFinish() {
+            counterState = false
+            Log.i("dam2", "mensajito")
         }
     }
 
+    fun toggle() {
+        Log.i("dam2", "toggle: $counterState")
+        if (this.counterState){
+            this.cancel()
+
+        } else {
+            Log.i("dam2", "toggle: start")
+            this.start()
+        }
+    }
+
+    fun start() {
+        counterState = true
+        this.myCounter.start()
+    }
+    fun cancel() {
+        counterState = false
+        this.myCounter.cancel()
+    }
 }
